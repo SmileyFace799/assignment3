13900e615080faf680907824c0dc7ab3cbac0d12
SmileyFace799 <Henrikgran85@gmail.com>
2024-04-04 16:33:42 +0200

    Print more info, FCFS algorithm done

--- src/main/java/no/ntnu/os2024/group3/CpuScheduling.java
+++ src/main/java/no/ntnu/os2024/group3/CpuScheduling.java
@@ -6,63 +6,64 @@ import java.util.Scanner;
 
 public class CpuScheduling {
 
-  public static void fcfs(Process[] processes) {
-    Arrays.sort(processes, Comparator.comparingInt(Process::getArrivalTime));
-    int currentTime = 0;
-    int totalWaitingTime = 0;
-    int totalTurnaroundTime = 0;
+	public static void fcfs(Process[] processes) {
+		Arrays.sort(processes, Comparator.comparingInt(Process::getArrivalTime));
+		int currentTime = 0;
+		int totalWaitingTime = 0;
+		int totalTurnaroundTime = 0;
 
-    for (Process process : processes) {
-      if (currentTime < process.getArrivalTime()) {
-        currentTime = process.getArrivalTime();
-      }
-      process.setWaitingTime(currentTime - process.getArrivalTime());
-      process.setCompletionTime(currentTime + process.getBurstTime());
-      process.setTurnaroundTime(process.getCompletionTime() - process.getArrivalTime());
+		for (Process process : processes) {
+			if (currentTime < process.getArrivalTime()) {
+				currentTime = process.getArrivalTime();
+			}
+			process.setWaitingTime(currentTime - process.getArrivalTime());
+			process.setCompletionTime(currentTime + process.getBurstTime());
+			process.setTurnaroundTime(process.getCompletionTime() - process.getArrivalTime());
 
-      currentTime += process.getBurstTime();
+			currentTime += process.getBurstTime();
 
-      totalWaitingTime += process.getWaitingTime();
-      totalTurnaroundTime += process.getTurnaroundTime();
-    }
+			totalWaitingTime += process.getWaitingTime();
+			totalTurnaroundTime += process.getTurnaroundTime();
+		}
 
-    System.out.println("First Come First Serve (FCFS) Scheduling:");
-    for (Process process : processes) {
-      System.out.println(
-        "Process ID: " +
-        process.getProcessID() +
-        ", Waiting Time: " +
-        process.getWaitingTime() +
-        ", Turnaround Time: " +
-        process.getTurnaroundTime()
-      );
-    }
+		System.out.println("First Come First Serve (FCFS) Scheduling:");
+		for (Process process : processes) {
+			System.out.println("Process ID: "
+					+ process.getProcessID()
+					+ ", Waiting Time: "
+					+ process.getWaitingTime()
+					+ ", Turnaround Time: "
+					+ process.getTurnaroundTime()
+					+ ", Arrival Time: "
+					+ process.getArrivalTime()
+					+ ", Burst Time: "
+					+ process.getBurstTime()
+			);
+		}
 
-    System.out.println(
-      "Average Waiting Time: " + (double) totalWaitingTime / processes.length
-    );
-    System.out.println(
-      "Average Turnaround Time: " +
-      (double) totalTurnaroundTime /
-      processes.length
-    );
-  }
+		System.out.println(
+				"Average Waiting Time: " + (double) totalWaitingTime / processes.length
+		);
+		System.out.println(
+				"Average Turnaround Time: " +
+						(double) totalTurnaroundTime /
+								processes.length
+		);
+	}
 
-  public static void PreemptivePriority(Process[] processes) {
-    System.out.println(
-      "Preemptive Priority Scheduling is not implemented yet."
-    );
-  }
+	public static void PreemptivePriority(Process[] processes) {
+		System.out.println(
+				"Preemptive Priority Scheduling is not implemented yet."
+		);
+	}
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		int numberOfProcesses = 10;
+		Process[] fcfsProcesses = new Process[numberOfProcesses];
+		for (int i = 0; i < numberOfProcesses; i++) {
+			fcfsProcesses[i] = Process.generateRandomProcess(String.valueOf(i));
+		}
 
-    int numberOfProcesses = 10;
-    Process[] fcfsProcesses = new Process[numberOfProcesses];
-    for (int i = 0; i < numberOfProcesses; i++) {
-      fcfsProcesses[i] = Process.generateRandomProcess(String.valueOf(i));
-    }
-
-    fcfs(fcfsProcesses);
-  }
+		fcfs(fcfsProcesses);
+	}
 }
